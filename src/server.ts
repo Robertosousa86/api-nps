5b91eba7f71b48af78be2749af7034c16104bf29
Roberto Sousa <robertosousa186@gmail.com>
2021-03-02 01:08:32 -0300

    creating first route: users

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,12 @@
 import "reflect-metadata";
 import express from "express";
 import "./database/index";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello juba" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Dados enviados" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Server is running free!");
