f6fe28105640a4d571a10f9633d8f4b5543c7743
Roberto Sousa <robertosousa186@gmail.com>
2021-03-02 00:00:08 -0300

    creating first migration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
+// sempre vem primeiro
+import "reflect-metadata";
 import express from "express";
+import "./database/index";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello juba" });
+});
+
+app.post("/", (req, res) => {
+  return res.json({ message: "Dados enviados" });
+});
 
 app.listen(3000, () => {
   console.log("Server is running free!");
