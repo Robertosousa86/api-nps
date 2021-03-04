613d5b425d97b27e171fbff7d3b56cff5cdde364
Roberto Sousa <robertosousa186@gmail.com>
2021-03-04 20:26:12 -0300

    creating user model

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,31 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController {
   async create(req: Request, res: Response) {
-    const body = req.body;
-    console.log(body);
-    return res.send();
+    const { name, email } = req.body;
+
+    const usersRepository = getRepository(User);
+
+    const userAlredyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (userAlredyExists) {
+      return res.status(400).json({
+        error: "User alredy exists",
+      });
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return res.json(user);
   }
 }
 
